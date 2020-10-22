faa3f90e6c0fc88945c3ab7e5c48977912f52e3b
v.ignatov <v.ignatov@fintech.ru>
2020-10-22 14:27:38 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	//"net/http"
+)
+//import "os"
+
+func main () {
+	fmt.Println("Hello!")
+	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//	_, _ = fmt.Fprintf(w, "Hello World!")
+	//})
+	//http.ListenAndServe("http://electronshik.loc:80", nil)
+}
